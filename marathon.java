a7abdd7d9392cd4fbaa9230e363ac2557a70a0da
Punnnati <161058500+Punnnati@users.noreply.github.com>
2025-07-25 22:59:12 +0530

    Add files via upload

---
+++ marathon.java
@@ -0,0 +1,13 @@
+class Marathon{
+	public static void main(String[] args) {
+		 boolean hasMedical = true;
+	        int Age = 25;
+	        if (Age >= 18 && Age <= 45 && hasMedical) {
+	            System.out.println("Can participate");
+	        } else {
+	            System.out.println("Not eligible");
+	        }
+
+	}
+
+}
