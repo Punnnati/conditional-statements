2e90867288406d49be55944900a349a44b634c99
Punnnati <161058500+Punnnati@users.noreply.github.com>
2025-07-25 22:07:49 +0530

    Add files via upload

---
+++ temperature.java
@@ -0,0 +1,16 @@
+class temp{
+public static void main(String[]args){
+int temp=50;
+if(temp>=45){
+   System.out.println("Ram should stay home");
+}
+else if(45<temp&&temp>=30){
+   System.out.println("Ram should  carry water");
+}
+else{
+   System.out.println("Ram should go normally");
+}
+}
+}
+
+   
