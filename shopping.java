be4fdc6b2bf547c451c56edb6c4850d56bfa3589
Punnnati <161058500+Punnnati@users.noreply.github.com>
2025-07-25 23:21:43 +0530

    Add files via upload

---
+++ shopping.java
@@ -0,0 +1,16 @@
+class Shopping{
+public static void main(String[]args){
+     int amount=25000;
+     boolean ismember=true;
+
+if(amount >=15000 && ismember){
+ System.out.println("she gets 30% discount.");
+}
+else if(amount >=10000 && ismember){
+ System.out.println("she gets 20% discount.");
+}
+else{
+ System.out.println("she gets 20% discount.");
+}
+}
+}
